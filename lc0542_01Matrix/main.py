ea254d76738b8a1af936ebd94992c987c0d35700
emira <emira1.ibrahimovic@gmail.com>
2026-05-10 01:55:32 -0400

    second, correct but slow approach for 01Matrix

--- lc0542_01Matrix/main.py
+++ lc0542_01Matrix/main.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import queue
 # shenanegans to import utils
 # from pathlib import Path
 # import sys
@@ -10,74 +10,36 @@ from typing import List
 
 class Solution:
 
-
-    def get_dist_mat(self, mat: List[List[int]], dir: tuple[int, int], init: tuple[int, int], end_condition, boundary_condition, reset_indices):
-        n, m = len(mat), len(mat[0])
-        i, j = init
-        running_dist = 1e9
-        dists = [[1e9] * m for _ in range(n)]
-
-        while not end_condition(i, j):
-            # print(i, j)
-            if mat[i][j] == 0:
-                running_dist = 0
-            else:
-                running_dist += 1
-            
-            dists[i][j] = running_dist
-
-            i += dir[0]
-            j += dir[1]
-            if boundary_condition(i, j):
-                i, j = reset_indices(i, j)
-                running_dist = 1e9
-
-        return dists
-
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         n = len(mat)
         m = len(mat[0])
+        def valid(x, y):
+            return 0 <= x < n and 0 <= y < m
 
-        dists_u = self.get_dist_mat(
-            mat,
-            (-1, 0),
-            (n - 1, 0),
-            lambda i, j: (j >= m),
-            lambda i, j: (i < 0),
-            lambda i, j: (n - 1, j + 1)
-        )
-        dists_d = self.get_dist_mat(
-            mat,
-            (+1, 0),
-            (0, 0),
-            lambda i, j: (j >= m),
-            lambda i, j: (i >= n),
-            lambda i, j: (0, j + 1)
-        )
-        dists_l = self.get_dist_mat(
-            mat,
-            (0, -1),
-            (0, m - 1),
-            lambda i, j: (i >= n),
-            lambda i, j: (j < 0),
-            lambda i, j: (i + 1, m - 1)
-        )
-        dists_r = self.get_dist_mat(
-            mat,
-            (0, +1),
-            (0, 0),
-            lambda i, j: (i >= n),
-            lambda i, j: (j >= m),
-            lambda i, j: (i + 1, 0)
-        )
+        agenda = queue.Queue()
+        dists = [[1e9 + 10] * m for _ in range(n)]
+        for i, row in enumerate(mat):
+            for j, cell in enumerate(row):
+                if mat[i][j] == 0:
+                    dists[i][j] = 0
+                    agenda.put((i, j))
+        
 
-        # print(dists_u)
-        # print(dists_d)
-        # print(dists_l)
-        # print(dists_r)
-        return [[min(dist_u, dist_d, dist_l, dist_r) 
-                 for dist_u, dist_d, dist_l, dist_r in zip(row_u, row_d, row_l, row_r)]
-                 for row_u, row_d, row_l, row_r in zip(dists_u, dists_d, dists_l, dists_r)]
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        while not agenda.empty():
+            i, j = agenda.get()
+            # print(i, j)
+
+            for d1, d2 in dirs:
+                i_n, j_n = i + d1, j + d2
+                # if valid(i_n, j_n):
+                #     print(dists[i_n][j_n])
+                if valid(i_n, j_n) and dists[i_n][j_n] >= 1e9:
+                    dists[i_n][j_n] = 1 + dists[i][j]
+                    agenda.put((i_n, j_n))
+        
+        return dists
+        
 
 if __name__ == "__main__":
     sol = Solution()
